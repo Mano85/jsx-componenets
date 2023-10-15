21be6c92b6e392ae90ff33be7178af12096a8fcb
manoranjan rath <105553749+Mano85@users.noreply.github.com>
2023-10-15 20:38:08 +0530

    functional componenet created

---
+++ functional component/functional.js
@@ -0,0 +1,10 @@
+export  const Header =()=>{
+    return(
+        <div id='title'>
+        <h1>head1</h1>
+        <h2>head2</h2>
+        <h3>head3</h3>
+    </div>
+    )
+}
+export default Header;
